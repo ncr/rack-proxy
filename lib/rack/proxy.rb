ebc1c75632b308f451dda3256dcc5a86267b7271
Jacek Becela <jacek.becela@gmail.com>
2010-01-22 15:33:54 +0100

    Fix requests with body without content type

--- lib/rack/proxy.rb
+++ lib/rack/proxy.rb
@@ -31,10 +31,10 @@ module Rack
       target_request.initialize_http_header(extract_http_request_headers(source_request.env))
 
       # Setup body
-      if target_request.request_body_permitted?
+      if target_request.request_body_permitted? && source_request.body
         target_request.body_stream    = source_request.body
         target_request.content_length = source_request.content_length
-        target_request.content_type   = source_request.content_type
+        target_request.content_type   = source_request.content_type if source_request.content_type
       end
       
       # Create a streaming response (the actual network communication is deferred, a.k.a. streamed)
