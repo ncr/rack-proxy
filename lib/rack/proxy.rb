020324c3b646560d98bde45cb1f00970e5ef4767
Jacek Becela <jacek.becela@gmail.com>
2013-10-08 11:11:03 +0200

    bump

--- lib/rack/proxy.rb
+++ lib/rack/proxy.rb
@@ -5,7 +5,7 @@ module Rack
 
   # Subclass and bring your own #rewrite_request and #rewrite_response
   class Proxy
-    VERSION = "0.5.4"
+    VERSION = "0.5.5"
 
     # @option opts [String, URI::HTTP] :backend Backend host to proxy requests to
     def initialize(opts={})
